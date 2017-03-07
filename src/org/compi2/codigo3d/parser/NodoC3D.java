5efffb0bd7b998eda1d46b21262f4dfa5aabb36e
JorgeARH <jorge.arh88@gmail.com>
2017-03-06 23:46:45 -0600

    Hoja de Trabajo No. 2 Jorge Antonio Rompiche Herrera 201114124

---
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -0,0 +1,14 @@
+package org.compi2.codigo3d.parser;
+
+public class NodoC3D {
+    private String cad;
+
+    public NodoC3D(String cad) {
+        this.cad = cad;
+    }
+    
+    public String getCad(){
+        return cad;
+    }
+        
+}
